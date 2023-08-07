e566418c00499f09b6daa54f01ed442f5d5a9eeb
dition0221 <qjatn8180@naver.com>
2023-08-07 20:16:19 +0900

    -temporary laptop

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("Created ✔");
-    return () => console.log("destroyed ❌");
-  }, []);
-  return <h1>Hello !</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [myMoney, setMyMoney] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setMyMoney(Number(event.target.elements.money.value));
+  };
   return (
     <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <div>
+          <form onSubmit={onSubmit}>
+            <input
+              name="money"
+              type="number"
+              placeholder="Write your '$' and Enter !"
+            />
+          </form>
+          <select>
+            {coins.map((coin) => (
+              <option key={coin.id}>
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+                {myMoney ? ` // ${myMoney / coin.quotes.USD.price}` : null}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
     </div>
   );
 }
