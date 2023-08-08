c11960e6dd84109f48c5d1b27493a3c50ca74571
dition0221 <qjatn8180@naver.com>
2023-08-08 21:46:07 +0900

    23-08-08 / React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Components
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/*" element={<h1>Not Found: 404</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
