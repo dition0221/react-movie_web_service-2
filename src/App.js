183bf0fb9c6bb7f926db5c157be933acbbe33b66
dition0221 <qjatn8180@naver.com>
2023-08-08 20:55:28 +0900

    -temporary laptop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  // API
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  console.log(movies);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
